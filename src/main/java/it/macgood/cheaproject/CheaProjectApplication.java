012f57aecccdcfa2fb44f508ee9ce0866d278bdd
Bogdan <churyumov@mail.ru>
2023-02-05 01:45:51 +0300

    Init project

---
+++ src/main/java/it/macgood/cheaproject/CheaProjectApplication.java
@@ -0,0 +1,13 @@
+package it.macgood.cheaproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheaProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CheaProjectApplication.class, args);
+    }
+
+}
